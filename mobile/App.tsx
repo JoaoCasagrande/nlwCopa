b7c4ed6dea64cbeed93674495e76850ccf8ba711
João Casagrande <pngbighouse@gmail.com>
2022-11-04 16:50:34 -0300

    Finalização da versão mobile - Aula 1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { Center, NativeBaseProvider, Text } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
-        <Text color="white" fontSize={24}>
-          Hello react-native
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {
+        fontsLoaded ? <SignIn /> : <Loading />
+      }
     </NativeBaseProvider>
   );
 }
