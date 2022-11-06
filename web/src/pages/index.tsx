d5c6478a0cca9431adf07710316c0cf247d70e08
João Gabriel Casagrande <pngbighouse@gmail.com>
2022-11-06 17:14:31 -0300

    Layout web da home page finalziado

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,123 @@
+import appPreviewImg from '../assets/app_preview.png';
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/avatares.png'
+import Image from 'next/image';
+import iconCheckImg from '../assets/check_icon.svg'
+import { api } from '../lib/axios';
+import { FormEvent, useState } from 'react';
+
+interface HomeProps {
+  poolCount: number;
+  guessCount: number;
+  userCount: number
+}
+
+export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle
+      })
+
+      const { code } = response.data
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso! O código foi copiado para a área de transferência.')
+      setPoolTitle('')
+    }
+    catch (err) {
+      console.log(err)
+      alert('Falha ao tentar criar o bolão. Tente novamente!')
+    }
+  }
+
+  return (
+    <div className="max-w-[1124px] h-screen pt-16 mx-auto grid grid-cols-2 gap-28 items-center" >
+      <main>
+        <Image src={logoImg} alt="NWL Copa Logo" />
+
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatarExampleImg} alt="Avatares exemplo" />
+
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">
+              +{props.userCount}
+            </span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border-gray-600 text-sm text-gray-100"
+            type="text"
+            placeholder="Qual é o nome do seu bolão?"
+            required
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
+          />
+          <button className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700" type="submit">
+            Criar meu bolão
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex justify-between text-gray-100">
+          <div className="flex items-center gap-6 mb-24">
+            <Image src={iconCheckImg} alt="Check" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+
+          <div className="w-px h-12 bg-gray-600"></div>
+
+          <div className="flex items-center gap-6 mb-24">
+            <Image src={iconCheckImg} alt="Check" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo a prévia da aplicação movel"
+        quality={100}
+      />
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const [
+    poolCountResponse,
+    guessCountResponse,
+    userCountResponse
+  ] = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count')
+  ])
+
+  return {
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count
+    }
+  }
+}
